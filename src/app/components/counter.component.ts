2c9c72915dbdbc4944838fed790ddc2521e3d99e
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-19 21:49:53 -0500

    usando reducer y actions para manejar el contador

--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../store/items.action';
 
 @Component({
   selector: 'app-counter',
@@ -12,18 +14,27 @@ export class CounterComponent {
 
   counter: number = 0;
 
+  constructor(private store: Store<{counter: number}> ) {
+    this.store.select('counter').subscribe(counter => {
+      this.counter = counter;
+    })
+  }
+
   increment(): void{
-    this.counter++
+    this.store.dispatch(increment({add : 4}));
+    // this.counter++
     console.log("Incrementando");
   }
 
   decrement(): void{
-    this.counter--
+    this.store.dispatch(decrement());
+    // this.counter--
     console.log("decrementando");
   }
 
   reset(): void{
-    this.counter = 0;
+    this.store.dispatch(reset());
+    // this.counter = 0;
     console.log('resetenado')
   }
 
